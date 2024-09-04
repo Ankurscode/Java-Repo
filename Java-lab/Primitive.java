81fc167a50d1496d50b89da7570478f44e350333
Ankur yadav <165983779+Ankurscode@users.noreply.github.com>
2024-09-04 10:56:50 +0530

    Add files via upload

---
+++ Java-lab/Primitive.java
@@ -0,0 +1,20 @@
+public class Primitive {
+    static byte defultByte;
+    static short defultShort;
+    static int defultInt;
+    static float defultFloat;
+    static double defultDouble;
+    static char defultChar;
+    static boolean defultBoolean;
+
+    public static void main(String[] args) {
+        System.out.println("Defult value of Byte:"+defultByte);
+        System.out.println("Defult value of Short:"+defultShort);
+        System.out.println("Defult value of Interger:"+defultInt);
+        System.out.println("Defult value of Float:"+defultFloat);
+        System.out.println("Defult value of Double:"+defultDouble);
+        System.out.println("Defult value of Character '' :"+defultChar);
+        System.out.println("Defult value of Boolean:"+defultBoolean);
+
+    }
+}
